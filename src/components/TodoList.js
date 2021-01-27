4d6ed79093586023fb095225fd8daf55116e2e73
Jesús Garrido <jesus.garrido@technest.es>
2021-01-27 11:40:01 +0100

    First action Add Todo on list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,23 @@
-import React, { PropTypes } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import Todo from "./Todo";
 
-const TodoList = ({ todos, onTodoClick }) => (
+const TodoList = ({ todos }) => (
   <ul>
     {todos.map((todo) => (
-      <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+      <Todo key={todo.id} {...todo} />
     ))}
   </ul>
 );
 
 TodoList.propTypes = {
-  //   todos: PropTypes.arrayOf(
-  //     PropTypes.shape({
-  //       id: PropTypes.number.isRequired,
-  //       completed: PropTypes.bool.isRequired,
-  //       text: PropTypes.string.isRequired,
-  //     }).isRequired
-  //   ).isRequired,
-  //   onTodoClick: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export default TodoList;
