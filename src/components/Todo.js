0451c86125b204448191df4a71ae9cbd0a6115bc
Jesús Garrido <jesus.garrido@technest.es>
2021-01-28 21:20:38 +0100

    Removed unused

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Todo = ({ onClick, completed, text }) => (
+const Todo = ({ completed, text, id }) => (
   <li
-    onClick={onClick}
+    onClick={(e) => {}}
     style={{
       textDecoration: completed ? "line-through" : "none",
     }}
@@ -13,9 +13,9 @@ const Todo = ({ onClick, completed, text }) => (
 );
 
 Todo.propTypes = {
-  //   onClick: PropTypes.func.isRequired,
   //   completed: PropTypes.bool.isRequired,
   //   text: PropTypes.string.isRequired,
+  //   id: PropTypes.number.isRequired,
 };
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Todo from "./Todo";
 
-const TodoList = ({ todos, onTodoClick }) => (
+const TodoList = ({ todos }) => (
   <ul>
     {todos.map((todo) => (
-      <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+      <Todo key={todo.id} {...todo} />
     ))}
   </ul>
 );
-
-TodoList.propTypes = {
-  //   todos: PropTypes.arrayOf(
-  //     PropTypes.shape({
-  //       id: PropTypes.number.isRequired,
-  //       completed: PropTypes.bool.isRequired,
-  //       text: PropTypes.string.isRequired,
-  //     }).isRequired
-  //   ).isRequired,
-  //   onTodoClick: PropTypes.func.isRequired,
-};
 
 export default TodoList;
